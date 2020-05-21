37d03622582d8f08b116d47708e8f6501b3418d9
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 12:22:42 -0500

    enhance: use env var for api key

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,21 +1,30 @@
-const request = require('request');
+const request = require("request");
 
 const geocode = (address, callback) => {
-    url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoianNobW9uZXkiLCJhIjoiY2p0MGx1eDEyMDl2MzQ5cDltdTY3NXV3ZyJ9._z08AfC1lj0ZCKtE8knZnQ`
-    request.get({url, json: true},  (err, {body})=>{
-        if(err){
-            callback('GEOCODE ERROR', undefined);
-        }else if(body.error || !body.hasOwnProperty('features') || body.features.length === 0){
-            callback('No location found', undefined)
-        }else{
-            let data = body.features[0];
-            callback(undefined, {
-                latitude: data.center[1],
-                longitude: data.center[0],
-                location: data.place_name
-            });
-        }
-    })
-}
+  const token = process.env.GEOCODE_API_KEY;
+  if (!token) return callback("[Geocode]: No Api key found.");
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${token}`;
+  request.get({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback("GEOCODE ERROR", undefined);
+    } else if (
+      body.error ||
+      !body.hasOwnProperty("features") ||
+      body.features.length === 0
+    ) {
+      console.log(body);
+      callback("No location found", undefined);
+    } else {
+      let data = body.features[0];
+      callback(undefined, {
+        latitude: data.center[1],
+        longitude: data.center[0],
+        location: data.place_name,
+      });
+    }
+  });
+};
 
 module.exports = geocode;
