3bd5a1b49f9865e6178a2625313d6e94f55b794a
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 14:50:30 -0500

    refactor: promisify requests and standardize errors

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,35 @@
 const request = require("request");
 
-const forecast = (lat, long, location, callback) => {
-  const token = process.env.FORECAST_API_KEY;
-  if (!token) return callback("[Weather]: No API key found.");
-  const url = `https://api.darksky.net/forecast/${token}/${lat},${long}`;
-  request.get({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback("FORECAST ERROR");
-      // console.error('Get Weather Errror: ', err);
-    } else if (body.error) {
-      callback("Couldnt get weather");
-    } else {
-      const data = body;
-      const { summary } = data.daily;
-      let msg = `It is currently ${data.currently.temperature} degrees and there is a ${data.currently.precipProbability}% chance of rain in ${location}.`;
-      callback(undefined, summary + "\n" + msg);
-    }
+const fetchForecast = (latitude, longitude) => {
+  return new Promise((resolve, reject) => {
+    const token = process.env.FORECAST_API_KEY;
+    if (!token) return callback("[Weather]: No API key found.");
+    const url = `https://api.darksky.net/forecast/${token}/${latitude},${longitude}`;
+    console.log(token, url);
+    request.get({ url, json: true }, (error, { statusCode, body }) => {
+      // if request error
+      if (error || body.error) {
+        return reject({
+          status: statusCode || 500,
+          body: {
+            message: error ? "FORECAST ERROR" : "Couldnt get weather",
+            error: error || body.error,
+          },
+        });
+      }
+      // if shape is incorrect
+      if (!body.daily) {
+        return reject({
+          status: 404,
+          body: {
+            message: "Couldnt get daily weather for location",
+            error: body,
+          },
+        });
+      }
+      resolve(body);
+    });
   });
 };
 
-module.exports = forecast;
+module.exports = fetchForecast;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,30 +1,36 @@
 const request = require("request");
 
-const geocode = (address, callback) => {
-  const token = process.env.GEOCODE_API_KEY;
-  if (!token) return callback("[Geocode]: No Api key found.");
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-    address
-  )}.json?access_token=${token}`;
-  request.get({ url, json: true }, (err, { body }) => {
-    if (err) {
-      callback("GEOCODE ERROR", undefined);
-    } else if (
-      body.error ||
-      !body.hasOwnProperty("features") ||
-      body.features.length === 0
-    ) {
-      console.log(body);
-      callback("No location found", undefined);
-    } else {
-      let data = body.features[0];
-      callback(undefined, {
-        latitude: data.center[1],
-        longitude: data.center[0],
-        location: data.place_name,
+const fetchGeocode = (address) => {
+  return new Promise((resolve, reject) => {
+    // get Token from env vars
+    const TOKEN = process.env.GEOCODE_API_KEY;
+    if (!TOKEN) return reject("[Geocode]: No Api key found.");
+
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${TOKEN}`;
+    request.get({ url, json: true }, (err, { statusCode, body }) => {
+      // if error
+      if (err || body.error) {
+        return reject({
+          status: statusCode || 500,
+          body: { message: "GEOCODE ERROR", error: err || body.error },
+        });
+      }
+      // if not expected shape
+      if (!body.hasOwnProperty("features") || body.features.length === 0) {
+        return reject({
+          status: 404,
+          body: { message: "No location found", error: body },
+        });
+      }
+
+      const location = body.features[0];
+      resolve({
+        latitude: location.center[1],
+        longitude: location.center[0],
+        location: location.place_name,
       });
-    }
+    });
   });
 };
 
-module.exports = geocode;
+module.exports = fetchGeocode;
